6f0608f82ae488965de0cc0773c13342eb107f7d
brenobss <brenobarreto5@hotmail.com>
2022-07-22 21:13:45 -0300

    create toobar view

--- src/view/MainScreen.java
+++ src/view/MainScreen.java
@@ -1,21 +1,26 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JToolBar;
-import javax.swing.JTextField;
-import java.awt.Color;
+import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.ImageIcon;
 
-public class MainScreen extends JFrame {
+public class MainScreen {
 
-	private JPanel contentPane;
+	private JFrame frame;
+	private final JPanel panel = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -24,8 +29,8 @@ public class MainScreen extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainScreen frame = new MainScreen();
-					frame.setVisible(true);
+					MainScreen window = new MainScreen();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -34,27 +39,38 @@ public class MainScreen extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public MainScreen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 813, 460);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(34, 139, 34));
-		panel.setName("");
-		contentPane.add(panel, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel_1 = new JLabel("Todo App");
-		lblNewLabel_1.setFont(new Font("Segoe UI", Font.PLAIN, 36));
-		panel.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel = new JLabel("Anote tudo, não esqueça de nada");
-		panel.add(lblNewLabel);
+		initialize();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 608, 423);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		panel.setBackground(new Color(60, 179, 113));
+		panel.setBounds(0, 0, 592, 115);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Todo App");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\breno\\Documents\\Workspace\\todo-java\\resources\\tick.png"));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(10, 11, 582, 65);
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 36));
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Anote tudo, não esqueça de nada");
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(10, 81, 582, 20);
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.BOTTOM);
+		panel.add(lblNewLabel_1);
+	}
 }
